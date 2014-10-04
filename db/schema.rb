3d24e572d238a7468f29af2f854879ff86837f3c
Yonatan <yvalenta@gmail.com>
2014-10-04 13:42:50 -0500

    Faltante

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20141004114348) do
+ActiveRecord::Schema.define(version: 20141004114659) do
 
   create_table "empresas", force: true do |t|
     t.string   "nombre"
